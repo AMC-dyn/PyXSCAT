f0bd2ae9cc541d6d957145c9c0901c9f65f38511
Simmermacher <m.simmermacher@ed.ac.uk>
2021-08-13 13:57:04 +0100

    subroutine ismember should work

--- src/maricones.f95
+++ src/maricones.f95
@@ -3,13 +3,15 @@
 ! Andres Moreno Carrascosa and Mats Simmermacher, 2021
 !-----------------------------------------------------------------------
 
-module types
+MODULE types
     implicit none
         INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(15)
         INTEGER, PARAMETER :: ikind = SELECTED_INT_KIND(8)
-end module types
+END MODULE types
 
-module twordmshit
+
+
+MODULE twordmshit
 
     implicit none 
 
@@ -18,27 +20,31 @@ module twordmshit
 
 
     SUBROUTINE ismember(col,matrix,memval,colnum)
+
         use types
+    
+        integer(kind=ikind), intent(in), dimension(4)        :: col
+        integer(kind=ikind), intent(in), dimension(:,:)      :: matrix
+        integer(kind=ikind), dimension(size(matrix(:,1)))    :: subcol
+        logical, intent(out)                                 :: memval
+        integer(kind=ikind), intent(out)                     :: colnum
 
-        integer(kind=ikind), intent(in), dimension(4)      :: col
-        integer(kind=ikind), intent(in), dimension(:,4)    :: matrix
-        integer(kind=ikind), dimension(size(matrix(:,1)))  :: subcol
-        logical, intent(out)                               :: memval
-        integer(kind=ikind), intent(out)                   :: colnum
+        subcol = sum(abs(matrix - spread(col, 1, size(matrix(:,1)))), dim=2)
+        colnum = minloc(subcol, dim=1)
 
-        subcol = sum(abs(matrix - spread(col, 1, size(matrix(:,1)))), dim=1)
-        colnum = findloc(subcol, 0)
-        if colnum == 0 then
-            memval = .true.
+        if (subcol(colnum) == 0) then
+           memval = .true.
         else
-            memval = .false.
-        endif            
+           colnum = -1
+           memval = .false.
+        endif
 
     END SUBROUTINE
 
 
 
     SUBROUTINE whateverthename(whateverthevariables)
+
         use types
 
         newtotal = []
@@ -48,23 +54,22 @@ module twordmshit
             do j = 1, n
                 do k = 1, n 
                     do l = 1, n
-                        call ismember((/i,j,k,l/),matst,memval,colnum)
-                        if not memval then
-                            call ismember((/j,i,l,k/),matst,memval,colnum)
-                            if memval then
-                                newtotal = [newtotal;totalst(colnum)]
-                                newmat = [newmat;[j i l k]]
-                            else
-                                call ismember((/l,k,j,i/)
-                            elseif ismember([l,k,j,i],matst,'rows') then
-                                [~,num] = ismember([l,k,j,i],matst,'rows')
-                                newtotal = [newtotal;totalst(num)]
-                                newmat = [newmat;[l k j i]]
-                            endif
+                        call ismember((/ i, j, k, l /), matst, memb, num)
+                        if (memb) then 
+                            newtotal = [newtotal; totalst(num)]
+                            newmat = [newmat; [i j k l]]
                         else
-                            [~,num] = ismember([i,j,k,l],matst,'rows')
-                            newtotal = [newtotal;totalst(num)]
-                            newmat = [newmat;[i j k l]]
+                            call ismember((/ j, i, l, k /), matst, memb, num)
+                            if (memb) then
+                                newtotal = [newtotal; totalst(num)]
+                                newmat = [newmat; [j i l k]]
+                            else
+                                call ismember((/ l, k, j, i /), matst, memb, num)
+                                if (memb) then
+                                    newtotal = [newtotal; totalst(num)]
+                                    newmat = [newmat; [l k j i]]
+                                endif
+                            endif
                         endif
                     enddo
                 enddo
@@ -81,7 +86,7 @@ module twordmshit
             mo3 = mat2(cc,3)
             mo4 = mat2(cc,4)
             B = [mo mo2 mo3 mo4]
-            [tf, ~]=ismember(newmat,mat2(cc,:),'rows')
+            call ismember(newmat, mat2(cc,:), tf, num)
             m1(cc) = mo
             m2(cc) = mo2
             m3(cc) = mo3
@@ -91,3 +96,10 @@ module twordmshit
         
         newmat = [m1' m2' m3' m4']
         newtotal = total2'
+   
+    END SUBROUTINE
+
+
+
+END MODULE
+
