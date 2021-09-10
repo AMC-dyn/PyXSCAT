73eb2db05da9b15b5a33cf4782c4b52836f28f5a
AMC-Dyn <andresmor89@gmail.com>
2021-09-10 16:37:43 +0100

    All good up to elastic

---
+++ src/script_compilation.sh
@@ -0,0 +1,15 @@
+
+#/bin/bash 
+
+f1='ifort -O3 -fPIC -fopenmp -heap-arrays -mkl -c'
+
+rm -rf *.mod *.o
+
+$f1 unique_module.f90
+$f1 twordms.f90
+$f1 one_rdm.f90
+$f1 Integrals.f90
+$f1 Variables.f90
+
+f2py3 -c --fcompiler=ifort --f90flags='-qopenmp -mkl  -heap-arrays ' -I.  *.o main_calculation.f90 -m integrals_wrapper
+
