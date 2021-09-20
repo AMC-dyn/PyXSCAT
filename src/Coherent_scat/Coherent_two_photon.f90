1921bfcbc72af2dc384f3494acf775b94dc87f1f
AMC-dyn <andresmor89@gmail.com>
2021-09-20 13:48:08 +0100

    one rdm working

--- src/Coherent_scat/Coherent_two_photon.f90
+++ src/Coherent_scat/Coherent_two_photon.f90
@@ -8,42 +8,77 @@ Program Coherent_scattering
 
         integer(kind=ikind) ::natoms,i,j,count
 
-        real(kind=dp),dimension(:),allocatable:: bond,bondabs
+        real(kind=dp),dimension(:),allocatable:: bond
+
+        real(kind=dp),dimension(:,:),allocatable::vbond,multaff1,multaff2
         real(kind=dp), dimension(nq):: q1,q2
         character(len=10),dimension(:), allocatable ::atoms
         real(kind=dp),dimension(:),allocatable :: x,y,z
-        real(kind=dp),dimension(:,:),allocatable:: aff1,aff2
+        real(kind=dp),dimension(:,:),allocatable:: aff1,aff2,iff1,iff2
         real(kind=dp),dimension(:),allocatable ::ff
         character(len=10),dimension(151):: atoms_table
+        character(len=10),dimension(55) :: atoms_table_2
+        real(kind=dp),dimension(12):: q_table_2
+        real(kind=dp),dimension(55,12):: iff
+        real(kind=dp),dimension(nq) :: iff_fin
+        real(kind=dp),dimension(:,:), allocatable :: iff_interp
         real(kind=dp), dimension(4,151) :: a,b
         real(kind=dp),  dimension(151):: c
+        real(kind=dp), dimension(nq) :: q12p, q12m,f1,f2,s1,s2
+        real(kind=dp),dimension(180) :: angleq
+        real(kind=dp),dimension(nq,nq,180) :: sigma
+        real(kind=dp) :: anglebond
 
 
 
         call linspace(0.00001_dp,4.0_dp,nq,q1)
         call linspace(0.00001_dp,4.0_dp,nq,q2)
+        call linspace(0.00001_dp,pi,180,angleq)
 
         open(unit=16,file="mol.xyz")
         read(16,*)natoms
 
         allocate(atoms(natoms), x(natoms), y(natoms), z(natoms),aff1(nq,natoms),aff2(nq,natoms),ff(nq))
-        allocate(bond((natoms**2-natoms)/2))
+        allocate(bond((natoms**2-natoms)/2),vbond(3,(natoms**2-natoms)/2),multaff1(nq,(natoms**2-natoms)/2))
+        allocate(multaff2(nq,(natoms**2-natoms)/2),iff1(nq,natoms),iff2(nq,natoms), multiff1(nq,(natoms**2-natoms)/2))
+        allocate(multiff2(nq,(natoms**2-natoms)/2))
         call table_of_ff("form_factors.dat",atoms_table,a,b,c)
+        call table_of_iff("incoherent_factors.dat",atoms_table_2,iff,q_table_2)
+
+
         do i=1,natoms
             read(16,*)atoms(i),x(i),y(i),z(i)
             call obtain_form_factors(ff,atoms(i),q1,atoms_table,a,b,c)
             aff1(:,i)=ff
             call obtain_form_factors(ff,atoms(i),q2,atoms_table,a,b,c)
             aff2(:,i)=ff
+            call obtain_i_form_factors(atoms_table_2,atoms(i),iff, q1,q_table_2,iff_fin)
+            iff1(:,i)=iff_fin
+            call obtain_i_form_factors(atoms_table_2,atoms(i),iff, q2,q_table_2,iff_fin)
+            iff2(:,i)=iff_fin
+
 
         end do
+        close(16)
+        f1=sum(abs(aff1)**2.0_dp,dim=2)
+        f2=sum(abs(aff2)**2.0_dp,dim=2)
+        s1=sum(iff1,dim=2)
+        s2=sum(iff2,dim=2)
         count=0
         do i=1,natoms
             do j=i+1,natoms
-                bond(count)=
+                bond(count)=norm2((/x(i),y(i),z(i)/)-(/x(j),y(j),z(j)/))
+                vbond(:,count)=(/x(i),y(i),z(i)/)-(/x(j),y(j),z(j)/)
+                multaff1(:,count)= aff1(:,i)*aff1(:,j)
+                multaff2(:,count)= aff2(:,i)*aff2(:,j)
+                multiffi1(:,count)= iff1(:,i)*iff1(:,j)
+                multiff2(:,count)= iff2(:,i)*iff2(:,j)
+                count=count+1
             end do
         end do
-        close(16)
+
+
+
 
 
 
--- src/Coherent_scat/calculate_form_factors.f90
+++ src/Coherent_scat/calculate_form_factors.f90
@@ -1,4 +1,5 @@
 Module calculate_form_factors
+    use interp1D
     implicit none
     contains
 
@@ -51,6 +52,108 @@ end subroutine
 
     end subroutine obtain_form_factors
 
+    subroutine obtain_i_form_factors(atoms,atom,iff_or, q1,q_or,iff)
+        use interp1D
+        implicit none
+        INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(15)
+        INTEGER, PARAMETER :: ikind = SELECTED_INT_KIND(8)
+        real(kind=dp), parameter :: pi=dacos(-1.00_dp)
+        character(len=10), intent(in) :: atom
+        real(kind=dp), intent(in), dimension(12)::q_or
+        real(kind=dp), intent(in), dimension(:) :: q1
+        real(kind=dp), intent(in), dimension(:,:) :: iff_or
+        real(kind=dp),intent(out),dimension(:):: iff
+        character(len=10),intent(in),dimension(55):: atoms
+
+        integer(kind=ikind) :: index,i
+
+
+
+
+
+
+
+        do i=1,55
+           if (atom==atoms(i)) then
+               index=i
+            end if
+
+        end do
+        print*,index,atom
+        do i=1,12
+            print*,q_or(i), iff_or(index,i)
+        end do
+        iff=interp_linear_vec(q_or,iff_or(index,:), q1)
+
+    end subroutine obtain_i_form_factors
+
+      function interp_linear_vec(x,y,xout) result(yout)
+        ! Interpolate y from ordered x to ordered xout positions
+
+        implicit none
+
+        double precision, dimension(:), intent(IN) :: x, y
+        double precision, dimension(:), intent(IN) :: xout
+        double precision, dimension(size(xout)) :: yout
+        integer :: i, j, n, nout
+
+        n    = size(x)
+        nout = size(xout)
+
+!         write(*,*) minval(x), maxval(x), n, nout
+
+        do i = 1, nout
+            if (xout(i) < x(1)) then
+                yout(i) = y(1)
+!                 write(*,*) 1, xout(i)
+            else if (xout(i) > x(n)) then
+                yout(i) = y(n)
+!                 write(*,*) 2, xout(i)
+            else
+                do j = 1, n
+                    if (x(j) >= xout(i)) exit
+                end do
+
+                if (j == 1) then
+                    yout(i) = y(1)
+!                     write(*,*) 3, xout(i)
+                else if (j == n+1) then
+                    yout(i) = y(n)
+!                     write(*,*) 4, xout(i)
+                else
+                    yout(i) = interp_linear_internal(x(j-1:j),y(j-1:j),xout(i))
+!                     write(*,*) 5, xout(i)
+                end if
+            end if
+        end do
+
+        return
+
+      end function interp_linear_vec
+
+       function interp_linear_internal(x,y,xout) result(yout)
+
+        implicit none
+         INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(15)
+        INTEGER, PARAMETER :: ikind = SELECTED_INT_KIND(8)
+        real(dp), intent(IN)  :: x(2), y(2), xout
+        real(dp) :: yout
+        real(dp) :: alph
+
+        if ( xout .lt. x(1) .or. xout .gt. x(2) ) then
+            write(*,*) "interp1: xout < x0 or xout > x1 !"
+            write(*,*) "xout = ",xout
+            write(*,*) "x0   = ",x(1)
+            write(*,*) "x1   = ",x(2)
+            stop
+        end if
+
+        alph = (xout - x(1)) / (x(2) - x(1))
+        yout = y(1) + alph*(y(2) - y(1))
+
+        return
+
+    end function interp_linear_internal
     subroutine table_of_ff(filename,atoms,a,b,c)
         implicit none
         INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(15)
@@ -66,11 +169,66 @@ end subroutine
 
         do i=1,151
             read(15,*)atoms(i),a(1,i) ,b(1,i),a(2,i),b(2,i),a(3,i),b(3,i),a(4,i),b(4,i),c(i)
-            print*,i
+
         end do
         close(unit=15)
     end subroutine table_of_ff
 
+    subroutine table_of_iff(filename,atoms,ff,q)
+        implicit none
+        INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(15)
+        INTEGER, PARAMETER :: ikind = SELECTED_INT_KIND(8)
+        integer :: i
+        character(len=40), intent(in) :: filename
+        character(len=10) :: dumm
+        character(len=10),intent(out), dimension(55):: atoms
+        real(kind=dp), intent(out),dimension(55,12) :: ff
+        real(kind=dp), intent(out),dimension(12):: q
+
+        open(unit=15,file= filename)
+        read(15,*)dumm,q(1:12)
+        do i=1,55
+            read(15,*)atoms(i),ff(i,:)
+        end do
+        close(unit=15)
+    end subroutine table_of_iff
+
+
+subroutine interp1( xData, yData, xVal, yVal )
+! Inputs: xData = a vector of the x-values of the data to be interpolated
+!         yData = a vector of the y-values of the data to be interpolated
+!         xVal  = a vector of the x-values where interpolation should be performed
+! Output: yVal  = a vector of the resulting interpolated values
+
+  implicit none
+  INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(15)
+  INTEGER, PARAMETER :: ikind = SELECTED_INT_KIND(8)
+  real(kind=dp), intent(in) :: xData(:), yData(:), xVal(:)
+  real(kind=dp), intent(out) :: yVal(:)
+  integer(kind=ikind) :: inputIndex, dataIndex
+  real(kind=dp) :: minXdata, minYdata, xRange, weight,maxXDATA
+
+  ! Possible checks on inputs could go here
+  ! Things you may want to check:
+  !   monotonically increasing xData
+  !   size(xData) == size(yData)
+  !   size(xVal) == size(yVal)
+
+  minXData = xData(1)
+  maxXData = xData(size(xData))
+  xRange = maxXData - minXData
+
+  do inputIndex = 1, size(xVal)
+      ! possible checks for out of range xVal could go here
+
+      ! this will work if x is uniformly spaced, otherwise increment
+      ! dataIndex until xData(dataIndex+1)>xVal(inputIndex)
+      dataIndex = floor((xVal(inputIndex)-minXData)/xRange);
+
+      weight = (xVal(inputIndex) - xData(dataIndex))/(xData(dataIndex+1)-xData(dataIndex));
+      yVal(inputIndex) = (1.0-weight)*yData(dataIndex) +weight*yData(dataIndex+1);
+  end do
+end subroutine
 
 
 
